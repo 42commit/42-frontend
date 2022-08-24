4b0667161e4ebcf9ed48df794dd7cea1d96f3b5a
pipipi666 <nnm107072@gmail.com>
2022-08-25 01:29:53 +0300

    tab2

--- src/components/Tab/tab.jsx
+++ src/components/Tab/tab.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import style from './tab.module.css';
 
-export default function Tab({ isActive = false, name }) {
+export default function Tab({ isActive = false, name, onClick }) {
     const [active, setActive] = useState(isActive)
     const className = `${style.tab} ${active ? style.active : style.inactive}`
     const handleClick = (e) => {
-        e.preventDefault();
+        onClick(e)
         setActive(true)
     }
     return (
